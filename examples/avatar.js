ee9b1073f62742721cee3630c0cc008d3c14dacb
ifyoucanhear ifyoucanhear <lucas.cavassani.q@gmail.com>
2025-01-06 12:26:00 -0300

    adição de comentários nos outros dois comandos de exemplo

--- examples/avatar.js
+++ examples/avatar.js
@@ -1,17 +1,28 @@
+/**
+ * código de exemplo que mostra como o bot menciona usuários nas mensagens
+ * e como acessar os avatares dos usuários
+ */
+
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.login("test@test.com", "password123456");
 
 bot.on("message", function(message) {
+    // reage com todas as mensagens cujo o conteúdo seja "$avatar"
     if (message.content === "$avatar") {
-        var user = message.author; // o usuário que deseja um avatar no autor
+        // obtém o usuário que solicitou seu avatar
+        var user = message.author;
 
+        // checa se o usuário de fato possui um avatar
         if (user.avatar) {
+            // constró o url do avatar do usuário com seu id
             var url = "https://discordapp.com/api/users/" + user.id + "/avatars/" + user.avatar + ".jpg";
 
+            // um usuário pode ser mencionado na mensagem inserindo a string obtida
             bot.sendMessage(message.channel, message.author.mention() + ", avatar: " + url);
         } else {
+            // nada deve ser feito caso o usuário não possua um avatar
             bot.sendMessage(message.channel, message.author.mention() + ", não possui um avatar...");
         }
     }
--- examples/presence.js
+++ examples/presence.js
@@ -1,8 +1,16 @@
+/**
+ * código de exemplo que mostra como o bot escuta pelos eventos de mudança
+ * de presença, como um usuário entrando ou deixando algum canal
+ */
+
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.login("test@test.com", "password123456");
 
+// o evento "presence" é alertado quando um usuário entra no servidor, deixa ou vai embora
 bot.on("presence", function(user, status, server) {
+    // envia uma mensagem no canal padrão do servidor, onde as
+    // atualizações de presença não estão desativadas
     bot.sendMessage(server.getDefaultChannel(), user.mention() + " is " + status + "!");
 });
