8a594041987a744fb0f8d718f83b425d0d9ae73e
ifyoucanhear ifyoucanhear <lucas.cavassani.q@gmail.com>
2025-01-07 09:44:31 -0300

    configuração básica de bot exemplar

---
+++ bot/README.md
@@ -0,0 +1,3 @@
+# bot
+
+open-source bot made with the intents of demonstrating the capabilities of [discord.js](https://github.com/ifyoucanhear/djs)
---
+++ bot/bot.js
@@ -0,0 +1,27 @@
+// caso o discord.js não tenha sido clonado, alterar o parâmetro
+// `require` para `discord.js` e então, rodar `npm install --save
+// discord.js` no mesmo diretório do arquivo. o bot irá rodar
+var Discord = require("../");
+
+// carregue o arquivo de configuração. caso ainda não tenha, crie
+// um que siga essa estrutura: { "email": "test@test.com", "password":
+// "password123456" }
+var BotConfig = require("./config.json");
+
+// cria um novo client do discord
+var bot = new Discord.Client();
+
+// log o cliente utilizando detalhes de auth no config.json
+bot.login(BotConfig.email, BotConfig.password + "a");
+
+console.log("inicializando tudo...");
+
+// quando o bot estiver pronto, output no console
+bot.on("ready", function() {
+    console.log("tudo pronto");
+});
+
+// quando o bot for desconectado, encerrar tudo
+bot.on("disconnected", function(obj) {
+    console.log("desconectado", obj.reason);
+});
