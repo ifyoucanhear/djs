8f4d30d757867c199ee29ecb6f233063e3471a6d
ifyoucanhear ifyoucanhear <lucas.cavassani.q@gmail.com>
2025-01-11 16:20:31 -0300

    métodos básicos adicionados

--- src/Client.js
+++ src/Client.js
@@ -3,6 +3,7 @@ var Endpoints = require("./Endpoints.js");
 
 // módulos node
 var request = require("superagent");
+var WebSocket = require("ws");
 
 var defaultOptions = {
     cache_tokens: false
@@ -21,6 +22,7 @@ class Client {
 		this.websocket = null;
 		this.events = new Map();
 		this.user = null;
+        this.alreadySentData = false;
 
         /**
          * valores de estado:
@@ -36,6 +38,8 @@ class Client {
     get ready() {
         return this.state === 3;
     }
+
+    debug(message) {}
 
     // login padrão
     login(email = "test@test.com", password = "password123456", callback = function() {}) {
@@ -57,9 +61,53 @@ class Client {
                     } else {
                         self.state = 2; // configura o estado como logado (ainda não pronto)
 
-                        self.token = res.body.token;
+                        self.token = res.body.token; // token configurado
+                        self.trySendConnData();
+
+                        callback(null, self.token);
                     }
                 });
+        }
+    }
+
+    // createws padrão
+    createws() {
+        if (this.websocket)
+            return false;
+
+        var self = this;
+
+        this.websocket = new WebSocket(Endpoints.WEBSOCKET_HUB);
+
+        this.websocket.onopen = function() {
+            self.trySendConnData(); // tentantiva de conexão
+        };
+    }
+
+    // trysendconndata padrão
+    trySendConnData() {
+        if (this.token && this.websocket.readyState === WebSocket.OPEN && !this.alreadySentData) {
+            this.alreadySentData = true;
+
+            var data = {
+                op: 2,
+
+                d: {
+                    token: this.token,
+
+					v: 2,
+
+					properties: {
+						"$os": "discord.js",
+						"$browser": "discord.js",
+						"$device": "discord.js",
+						"$referrer": "",
+						"$referring_domain": ""
+					}
+                }
+            };
+
+            this.websocket.send(JSON.stringify(data));
         }
     }
 }
