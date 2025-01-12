96f0d934c1e22c4eed692a873ea9ffa0d8109950
ifyoucanhear ifyoucanhear <lucas.cavassani.q@gmail.com>
2025-01-12 10:27:44 -0300

    credenciais corrigidas

--- test/bot.js
+++ test/bot.js
@@ -1,8 +1,9 @@
 var Discord = require("../lib/index");
 
+var Auth = require("./auth.json");
 var bot = new Discord.Client();
 
-bot.login("email", "pass", function(err, res) {});
+bot.login(Auth.email, Auth.password, function(err, res) {});
 
 bot.on("ready", function() {
     console.log("pronto!");
